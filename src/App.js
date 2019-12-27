412644fdb679157678b3717b496d6d92cf5d8484
Victor <adevic4christ@gmail.com>
2019-12-27 23:26:14 +0100

    cread app component, component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
+class App extends Component {
+ constructor() {
+  super();
+  this.state = { aliens: [] };
+ }
+
+ componentDidMount() {
+  fetch("https://jsonplaceholder.typicode.com/users")
+   .then(response => response.json())
+   .then(users => this.setState({ aliens: users }));
+ }
+
+ render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className="App">
+    {this.state.aliens.map(alien => (
+     <h1 key={alien.id}>{alien.name}</h1>
+    ))}
+   </div>
   );
+ }
 }
 
 export default App;
