031bfd524fd8e493d7fba151245ff6febcd4f3a6
Victor <adevic4christ@gmail.com>
2019-12-28 12:13:00 +0100

    Updates

---
+++ precache-manifest.18c3fe36bdc09c4b7d0eeb15ba0026b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e244fda217e72a764cc9bbfe16a9495e",
+    "url": "/aliens-rolodex/index.html"
+  },
+  {
+    "revision": "ce268665d01c7600189f",
+    "url": "/aliens-rolodex/static/css/main.9028160f.chunk.css"
+  },
+  {
+    "revision": "039a96ef6a8bdd80c1d2",
+    "url": "/aliens-rolodex/static/js/2.c34d4464.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/aliens-rolodex/static/js/2.c34d4464.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ce268665d01c7600189f",
+    "url": "/aliens-rolodex/static/js/main.ca229ec5.chunk.js"
+  },
+  {
+    "revision": "6cf8107cb36f3d3946cb",
+    "url": "/aliens-rolodex/static/js/runtime-main.ca66561f.js"
+  }
+]);
