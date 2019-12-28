f87529861df24ed04e5827eeb63c6f4e6a5ebbe5
Victor <adevic4christ@gmail.com>
2019-12-28 11:21:07 +0100

    added SearchBox component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { CardList } from "./components/card-list/card-list.component";
 import "./App.css";
+import SearchBox from "./components/search-box/search-box.component";
 
 class App extends Component {
  constructor() {
@@ -14,20 +15,23 @@ class App extends Component {
    .then(users => this.setState({ aliens: users }));
  }
 
+ handleChange = e => {
+  this.setState({ searchField: e.target.value });
+ };
+
  render() {
+  const { aliens, searchField } = this.state;
+  const filteredAliens = aliens.filter(alien =>
+   alien.name.toLowerCase().includes(searchField.toLowerCase())
+  );
   return (
    <>
     <div className="App">
-     <input
-      type="search"
-      placeholder="search aliens"
-      onChange={e => {
-       this.setState({ searchField: e.target.value }, () =>
-        console.log(this.state.searchField)
-       );
-      }}
-     />
-     <CardList aliens={this.state.aliens} />
+     <SearchBox onChange={this.handleChange} placeholder="search aliens" />
+     {!aliens.length && <h2>Loading...</h2>}
+     {/* {!filteredAliens.length && <h2>No Alien with the name {searchField}</h2>} */}
+     {/* <CardList aliens={filteredAliens.length ? filteredAliens : aliens} /> */}
+     <CardList aliens={filteredAliens} />
     </div>
    </>
   );
